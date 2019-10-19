0a96ff38554101bd5d235d613aaae048927e1353
Jessica Stapp <stapp9@gmail.com>
2019-10-19 13:49:12 -0700

    pull js file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,22 +1,25 @@
-
-$("#search-query").on("click", function (event) {
-    event.preventDefault();
-    searchTerm();
+$("#search-query").on("click", function(event) {
+  event.preventDefault();
+  searchTerm();
 });
 
 function searchTerm() {
+  var apiKey = "GSnWAsYEcZHxPE8fgBGeZeS7nTIpAoWR";
+  var searchQuery = $("#inputID")
+    .val()
+    .trim();
+  var queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    searchQuery +
+    "&api-key=" +
+    apiKey;
 
-    var apiKey = "GSnWAsYEcZHxPE8fgBGeZeS7nTIpAoWR";
-    var searchQuery = $("#inputID").val().trim();
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "&api-key=" + apiKey;
+  console.log(queryURL);
 
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    })
-
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
 }
